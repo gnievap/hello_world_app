90b60209f461584dfb616eec42722869be8696ea
Gaby Nieva <gaby.nieva@outlook.com>
2024-05-15 14:53:07 -0600

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counters/counter_screen.dart';
 
 void main(){
-  runApp( MyApp() );
+  runApp( const MyApp() );
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
+      debugShowCheckedModeBanner: false,
+      home:CounterScreen(),
     );
   }
 
